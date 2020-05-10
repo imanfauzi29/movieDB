63d6615c441d47644ccd95e00d8d035715a590e9
Iman Fauzi <imanfauzi29@gmail.com>
2020-05-10 12:49:10 +0700

    remove warning at run

--- webpack.common.js
+++ webpack.common.js
@@ -1,1 +1,32 @@
-const path = require('path');const HtmlPackPlugin = require('html-webpack-plugin');module.exports = {	entry: './src/index.js',	output: {		path: path.resolve(__dirname, 'dist'),		filename: 'bundle.js'	},	module: {		rules: [			{				test: /\.css$/,				use: [					{						loader: 'style-loader'					},					{						loader: 'css-loader'					}				]			}		]	},	plugins: [		new HtmlPackPlugin({			template: './src/index.html',			filename: 'index.html'		})	]}
+const path = require('path');
+const webpack = require('webpack');
+const HtmlPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  performance: {
+    hints: false
+  },
+  plugins: [
+    new HtmlPackPlugin({
+      template: './src/index.html',
+      filename: 'index.html'
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+    })
+  ]
+}
