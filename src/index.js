97e03aadfa7ee20e348c97cdc709537b8b7328e9
Iman Fauzi <imanfauzi29@gmail.com>
2020-05-10 12:51:51 +0700

    add new

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
-import './styles/style.css';
-import './scripts/main.js';
+import "bootstrap/dist/css/bootstrap.min.css";
+import $ from "jquery";
+import "./styles/style.css";
+import main from './scripts/main.js';
+import "./scripts/font-awesome.js";
+
+/*
+key: 18b6ac76ada34bba374b08f5932d3416;
+genre: https://api.themoviedb.org/3/genre/movie/list?api_key=18b6ac76ada34bba374b08f5932d3416&language=en-US
+list movie: https://api.themoviedb.org/3/list/2?page=1&api_key=18b6ac76ada34bba374b08f5932d3416;
+request token:  https://api.themoviedb.org/3/authentication/session/new?api_key=18b6ac76ada34bba374b08f5932d3416;
+*/
