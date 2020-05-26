a80c94bf336eb2158f96782d5288062b4ae320d9
Iman Fauzi <imanfauzi29@gmail.com>
2020-05-26 08:19:50 +0700

    ..

--- webpack.common.js
+++ webpack.common.js
@@ -1,30 +1,24 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlPackPlugin = require('html-webpack-plugin');
-const terserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: '[name].bundle.js'
   },
   module: {
     rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
+    {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    }
     ]
   },
-  plugins: [new HtmlPackPlugin({template: './src/index.html', filename: 'index.html'})],
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new terserPlugin({
-        test: /\.js(\?.*)?$/i,
-      })
-    ],
-    sourceMap: true
-  }
+  plugins: [
+  new HtmlPackPlugin({
+    template: './src/index.html',
+    filename: 'index.html'})
+  ]
 }
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,22 +1,27 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(common, {
 	mode: 'production',
 	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: [
-					{
-						loader: 'babel-loader',
-						options: {
-							presets: ["@babel/preset-env"]
-						}
-					}
-				]
-			}
-		]
-	}
+		rules: [{
+			test: /\.js$/,
+			exclude: /node_modules/,
+			use: [{
+				loader: 'babel-loader',
+				options: {
+					presets: ["@babel/preset-env"]
+				}
+			}]
+		}]
+	},
+	optimization: {
+		minimize: true,
+		minimizer: [
+		new TerserPlugin({
+			test: /\.js(\?.*)?$/i,
+		}),
+		],
+	},
 })
